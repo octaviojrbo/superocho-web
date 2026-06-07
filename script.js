22f50afca335eb987374f38a405357716e77e81f
octaviojrbo <octaviojrbo@gmail.com>
2026-06-07 16:29:03 -0600

    Correccion catalogo

--- script.js
+++ script.js
@@ -1008,3 +1008,14 @@ function setupProductViews(){
       observer.observe(card);
     });
 }
+
+const mobileMenuBtn = document.getElementById("mobileMenuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+
+if(mobileMenuBtn && mobileMenu){
+
+  mobileMenuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("active");
+  });
+
+}
